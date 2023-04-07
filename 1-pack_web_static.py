72e790ee153d853569cbbd4c634f552ac98a5f59
Brymo07 <felixowusu479@gmail.com>
2023-04-06 19:41:50 -0700

    airbnb_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" Program that compress a dir with tar before sending """
-from datetime import datetime
-from fabric.api import *
+from fabric.api import local
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """ Generates a .tgz archive from the contents of the web_static
-    folder of your AirBnB Clone repo """
-    date_str = datetime.now().strftime('%Y%m%d%H%M%S')
-    local("mkdir -p versions/")
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
     try:
-        local("tar -cvzf versions/web_static_{}.tgz web_static"
-              .format(date_str))
-        return "versions/web_static_{}.tgz".format(date_str)
-    except Exception:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
